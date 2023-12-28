63f3a4522fa15eb7489856f5c61cb1bbd978f1d0
martzcodes <978362+martzcodes@users.noreply.github.com>
2023-12-28 17:44:39 -0500

    cdc changes

--- src/fns/cdc-enrichment.ts
+++ src/fns/cdc-enrichment.ts
@@ -2,6 +2,7 @@ import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 import type { AttributeValue } from '@aws-sdk/client-dynamodb';
 import type { DynamoDBRecord } from 'aws-lambda';
+import { TABLE_PK, TABLE_SK } from '../constants';
 
 export const handler = async (records: DynamoDBRecord[]) => {
   return records.map((r) => {
@@ -11,9 +12,12 @@ export const handler = async (records: DynamoDBRecord[]) => {
     const newImage = r.dynamodb?.NewImage
       ? unmarshall(r.dynamodb?.NewImage as Record<string, AttributeValue>)
       : undefined;
-    const result: { dynamodb: Record<string, unknown>; eventName?: string } = {
+    const result: { pk: string; sk: string; dynamodb: Record<string, unknown>; eventName?: string; eventType: string; } = {
+      pk: oldImage?.[TABLE_PK] || newImage?.[TABLE_PK] || '',
+      sk: oldImage?.[TABLE_SK] || newImage?.[TABLE_SK] || '',
       dynamodb: {},
       eventName: r.eventName,
+      eventType: oldImage && newImage ? 'UPDATE' : oldImage ? 'REMOVE' : 'INSERT',
     };
     if (newImage) result.dynamodb.NewImage = newImage;
     if (oldImage) result.dynamodb.OldImage = oldImage;
--- src/stacks/wims-stack.ts
+++ src/stacks/wims-stack.ts
@@ -13,10 +13,9 @@ import {
 import { EventBus, Rule } from 'aws-cdk-lib/aws-events';
 import { SfnStateMachine } from 'aws-cdk-lib/aws-events-targets';
 import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
-import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
-import { Runtime, StartingPosition } from 'aws-cdk-lib/aws-lambda';
-import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { CfnPipe } from 'aws-cdk-lib/aws-pipes';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import {
@@ -39,9 +38,9 @@ import {
   PhysicalResourceId,
 } from 'aws-cdk-lib/custom-resources';
 import { Construct } from 'constructs';
-import { PROJECT_SOURCE } from '../constants';
 
-import { TABLE_PK, TABLE_SK } from '../constants';
+import { PROJECT_SOURCE, TABLE_PK, TABLE_SK } from '../constants';
+import { ObservabilityConstruct } from '../constructs/observability';
 
 export class WimsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -83,10 +82,10 @@ export class WimsStack extends Stack {
     });
 
     const table = new TableV2(this, 'WIMSTable', {
-      dynamoStream: StreamViewType.NEW_IMAGE,
-      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      dynamoStream: StreamViewType.NEW_AND_OLD_IMAGES,
+      partitionKey: { name: TABLE_PK, type: AttributeType.STRING },
       removalPolicy: RemovalPolicy.DESTROY,
-      sortKey: { name: 'sk', type: AttributeType.STRING },
+      sortKey: { name: TABLE_SK, type: AttributeType.STRING },
       tableName: 'WIMS',
     });
 
@@ -111,8 +110,8 @@ export class WimsStack extends Stack {
       },
       conditionExpression: '#quantity >= :quantity',
       key: {
-        pk: DynamoAttributeValue.fromString('INVENTORY#MACGUFFIN'),
-        sk: DynamoAttributeValue.fromString('MODEL#LX'),
+        [TABLE_PK]: DynamoAttributeValue.fromString('INVENTORY#MACGUFFIN'),
+        [TABLE_SK]: DynamoAttributeValue.fromString('MODEL#LX'),
       },
       resultPath: JsonPath.DISCARD,
       table,
@@ -167,23 +166,17 @@ export class WimsStack extends Stack {
     table.grantStreamRead(streamPipeRole);
     paymentsQueue.grantConsumeMessages(paymentsPipeRole);
 
-    const cdcFn = new NodejsFunction(this, 'ChangeDataCapture', {
-      entry: './src/fns/cdc.ts',
-      environment: {
-        TABLE_NAME: table.tableName,
-      },
-      functionName: 'cdc',
-      logRetention: RetentionDays.ONE_WEEK,
-      runtime: Runtime.NODEJS_20_X,
-    });
-    cdcFn.addEventSource(new DynamoEventSource(table, {
-      startingPosition: StartingPosition.LATEST,
-    }));
-    bus.grantPutEventsTo(cdcFn);
-
+    const cdcEvent = 'cdcEvent';
     new Rule(this, 'OrderStateMachineRule', {
       eventBus: bus,
-      eventPattern: { source: [PROJECT_SOURCE] },
+      eventPattern: {
+        source: [PROJECT_SOURCE],
+        detailType: [cdcEvent],
+        detail: {
+          eventType: ['INSERT'],
+          pk: ['CUSTOMER#*'],
+        },
+      },
       ruleName: 'OrdersStateMachine',
       targets: [new SfnStateMachine(sm)],
     });
@@ -207,6 +200,12 @@ export class WimsStack extends Stack {
         },
       },
       target: bus.eventBusArn,
+      targetParameters: {
+        eventBridgeEventBusParameters: {
+          source: PROJECT_SOURCE,
+          detailType: cdcEvent,
+        },
+      },
     });
 
     new CfnPipe(this, 'PaymentsPipe', {
@@ -271,10 +270,10 @@ export class WimsStack extends Stack {
           requestTemplates: {
             'application/json': JSON.stringify({
               Key: {
-                pk: {
+                [TABLE_PK]: {
                   S: 'INVENTORY#MACGUFFIN',
                 },
-                sk: {
+                [TABLE_SK]: {
                   S: 'MODEL#LX',
                 },
               },
@@ -310,10 +309,10 @@ export class WimsStack extends Stack {
           requestTemplates: {
             'application/json': JSON.stringify({
               Item: {
-                pk: {
+                [TABLE_PK]: {
                   S: "CUSTOMER#$input.path('$.customerId')",
                 },
-                sk: {
+                [TABLE_SK]: {
                   S: 'TIMESTAMP#$context.requestTimeEpoch',
                 },
                 customerId: {
@@ -361,6 +360,8 @@ export class WimsStack extends Stack {
         resources: [table.tableArn],
       }),
     });
+
+    new ObservabilityConstruct(this, 'Observability');
 
     new CfnOutput(this, 'OrdersUrl', {
       value: ordersApi.deploymentStage.urlForPath('/orders'),
